0659867f9c69a7720573430d2ce92619fea27116
ThorAtlas <jarredg2018@gmail.com>
2021-07-22 16:44:42 -0400

    Jarred's commit on maps

---
+++ app/src/main/java/edu/neu/madcourse/topdog/GPSPage/MapActivity.java
@@ -0,0 +1,16 @@
+package edu.neu.madcourse.topdog.GPSPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.neu.madcourse.topdog.*;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
