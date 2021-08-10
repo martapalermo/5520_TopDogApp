175d205e75bd97d62ab133c3b53a9bc9550037ca
martapalermo <marta.palermo421@gmail.com>
2021-08-10 11:02:44 -0700

    made edits to only have gallery work (camera code // out)

--- app/src/main/java/edu/neu/madcourse/topdog/MyProfile.java
+++ app/src/main/java/edu/neu/madcourse/topdog/MyProfile.java
@@ -1,15 +1,19 @@
 package edu.neu.madcourse.topdog;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.icu.text.SimpleDateFormat;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -22,7 +26,13 @@ import androidx.core.content.ContextCompat;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import org.jetbrains.annotations.NotNull;
 
 /**
  * NOTES: Need to tweak logic so that selected picture stays and doesnt reset everytime user exits
@@ -36,19 +46,19 @@ public class MyProfile extends AppCompatActivity {
 
     String username;
     ImageView selectedImage;
-    Button mGalleryBtn;
-    Button mCameraBtn;
+    Button mGalleryBtn, mCameraBtn;
     String currentPhotoPath;
     public static final int CAMERA_PERM_CODE = 101;
     public static final int CAMERA_REQ_CODE = 102;
-
-    private static final int IMAGE_PICK_CODE = 1000;
-    private static final int PERMISSION_CODE = 1001;
+    public static final int GALLERY_REQUEST_CODE = 105;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         username = getIntent().getStringExtra(MainActivity.USERKEY);
 
@@ -57,139 +67,188 @@ public class MyProfile extends AppCompatActivity {
         mGalleryBtn = findViewById(R.id.galleryBtn);
         mCameraBtn = findViewById(R.id.cameraBtn);
 
-        mGalleryBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // check runtime permission
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                            == PackageManager.PERMISSION_DENIED) {
-                        // permission denied, request it until accessed?
-                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-                        // show popup for runtime permission
-                        requestPermissions(permissions, PERMISSION_CODE);
-                    }
-                    else {
-                        // permission already granted
-                        pickImageFromGallery();
-                    }
+        // onclick listeners for buttons
+        mGalleryBtn.setOnClickListener(v -> {
+            try {
+                if (ActivityCompat.checkSelfPermission(MyProfile.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MyProfile.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, GALLERY_REQUEST_CODE);
                 } else {
-                    // system os is less than marshmallow
-                    pickImageFromGallery();
+                    openGallery();
                 }
-
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
 
-        mCameraBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // ask for runtime permission to use camera
-                askCameraPermission();
-            }
-        });
-    }
-
-    private void askCameraPermission() {
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            //request permission
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
-        }
-        else {
-            openCamera();
-        }
-    }
-
-    private void openCamera() {
-        // Toast.makeText(this, "Camera Open Request", Toast.LENGTH_SHORT).show();
-        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // opens default camera on phone
-        startActivityForResult(camera, CAMERA_REQ_CODE);
-    }
-
-    private void pickImageFromGallery() {
-        // intent to pick image
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, IMAGE_PICK_CODE);
-    }
-
-    // handle result of runtime permission
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == CAMERA_PERM_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                //dispatchTakePictureIntent();
-                openCamera();
-            }
-        }
-        else if (requestCode == IMAGE_PICK_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                pickImageFromGallery();
-            }
-        }
-        else {
-            // permission was denied
-            Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show();
-        }
-//        switch (requestCode) {
-//            case PERMISSION_CODE:{
-//                if (grantResults.length > 0 && grantResults[0] ==
-//                        PackageManager.PERMISSION_GRANTED) {
-//                    // permission was granted
+//        mGalleryBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // check runtime permission
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+//                            == PackageManager.PERMISSION_DENIED) {
+//                        // permission denied, request it until accessed?
+//                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+//                        // show popup for runtime permission
+//                        requestPermissions(permissions, PERMISSION_CODE);
+//                    }
+//                    else {
+//                        // permission already granted
+//                        pickImageFromGallery();
+//                    }
+//                } else {
+//                    // system os is less than marshmallow
 //                    pickImageFromGallery();
 //                }
-
+//
 //            }
-//        }
+//        });
+
+        mCameraBtn.setOnClickListener(v -> {
+            Toast.makeText(this, "Camera button clicked.", Toast.LENGTH_SHORT).show();
+            // ask for runtime permission to use camera
+            //askCameraPermission();
+        });
     }
 
-//    private void dispatchTakePictureIntent() {
-//    Intent takePicIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//    // ensure there is camera activity to handle intent
-//        if (takePicIntent.resolveActivity(getPackageManager()) != null) {
-//            // create file where photo should go
-//            File photoFile = null; // instantiate empty
-//            try {
-//                photoFile = createImageFile();
-//            } catch (IOException ex) {
-//                // error occurred while creating file
-//            }
-//            // if photo file is created
-//            if (photoFile != null) {
-//                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
-//                takePicIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-//                startActivityForResult(takePicIntent, CAMERA_REQ_CODE);
-//            }
-//        }
-//    }
-
-    private File createImageFile() throws IOException {
-        // create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
-
-        // save a file - path for use with action view intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-
+    public void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, GALLERY_REQUEST_CODE);
     }
-
-    // handle result of picked image
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == CAMERA_REQ_CODE || requestCode == PERMISSION_CODE) {
-            // set image to image view
-            //mImageView.setImageURI(data.getData());
-            Bitmap image = (Bitmap) data.getExtras().get("data");
-            selectedImage.setImageBitmap(image);
+        if (requestCode == GALLERY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri contentUri = data.getData();
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "PNG_" + timeStamp + "." + getFileExt(contentUri);
+                Log.d("tag", "onActivityResult: Gallery Image Uri: " + imageFileName);
+                selectedImage.setImageURI(contentUri);
+
+                uploadImageToFirebase(imageFileName, contentUri);
+            }
         }
     }
+
+    private void uploadImageToFirebase(String name, Uri contentURI) {
+        final StorageReference image = storageReference.child("pictures/" + name);
+        image.putFile(contentURI).addOnSuccessListener(taskSnapshot -> {
+            image.getDownloadUrl().addOnSuccessListener(uri -> Log.d("tag", "onSuccess: Upload Image URl is " + uri.toString()));
+
+            Toast.makeText(MyProfile.this, "Image Is Uploaded.", Toast.LENGTH_SHORT).show();
+        }).addOnFailureListener(e -> Toast.makeText(MyProfile.this, "Upload failed.", Toast.LENGTH_SHORT).show());
+    }
+
+    private String getFileExt(Uri contentUri) {
+        ContentResolver c = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(c.getType(contentUri));
+    }
+
+//    private void askCameraPermission() {
+//        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            //request permission
+//            ActivityCompat.requestPermissions(this, new String[]
+//                    {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+//        }
+//        else {
+//            openCamera();
+//        }
+//    }
+//    private void openCamera() {
+//        // Toast.makeText(this, "Camera Open Request", Toast.LENGTH_SHORT).show();
+//        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // opens default camera on phone
+//        startActivityForResult(camera, CAMERA_REQ_CODE);
+//    }
+
+//    private void pickImageFromGallery() {
+//        // intent to pick image
+//        Intent intent = new Intent(Intent.ACTION_PICK);
+//        intent.setType("image/*");
+//        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+//    }
+//
+//    // handle result of runtime permission
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+//                                           @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        if (requestCode == CAMERA_PERM_CODE) {
+//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                //dispatchTakePictureIntent();
+//                openCamera();
+//            }
+//        }
+//        else if (requestCode == GALLERY_REQUEST_CODE) {
+////            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+////                pickImageFromGallery();
+////            }
+//        }
+//        else {
+//            // permission was denied
+//            Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show();
+//        }
+////        switch (requestCode) {
+////            case PERMISSION_CODE:{
+////                if (grantResults.length > 0 && grantResults[0] ==
+////                        PackageManager.PERMISSION_GRANTED) {
+////                    // permission was granted
+////                    pickImageFromGallery();
+////                }
+//
+////            }
+////        }
+//    }
+//
+////    private void dispatchTakePictureIntent() {
+////    Intent takePicIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+////    // ensure there is camera activity to handle intent
+////        if (takePicIntent.resolveActivity(getPackageManager()) != null) {
+////            // create file where photo should go
+////            File photoFile = null; // instantiate empty
+////            try {
+////                photoFile = createImageFile();
+////            } catch (IOException ex) {
+////                // error occurred while creating file
+////            }
+////            // if photo file is created
+////            if (photoFile != null) {
+////                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile);
+////                takePicIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+////                startActivityForResult(takePicIntent, CAMERA_REQ_CODE);
+////            }
+////        }
+////    }
+//
+////    private File createImageFile() throws IOException {
+////        // create an image file name
+////        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+////        String imageFileName = "JPEG_" + timeStamp + "_";
+////
+////        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+////        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+////
+////        // save a file - path for use with action view intents
+////        currentPhotoPath = image.getAbsolutePath();
+////        return image;
+////
+////    }
+//
+//    // handle result of picked image
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (resultCode == RESULT_OK && requestCode == CAMERA_REQ_CODE) {
+//            // set image to image view
+//            //mImageView.setImageURI(data.getData());
+//            Bitmap image = (Bitmap) data.getExtras().get("data");
+//            selectedImage.setImageBitmap(image);
+//        }
+//    }
 
     @Override
     public void onBackPressed() {
