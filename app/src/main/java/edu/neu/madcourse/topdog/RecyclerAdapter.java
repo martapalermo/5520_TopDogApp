5d4703ac4a4774b7cb8a516119cfb53edabcf1d0
ThorAtlas <jarredg2018@gmail.com>
2021-08-12 23:15:29 -0400

    Fixed the distance/duration swap

--- app/src/main/java/edu/neu/madcourse/topdog/RecyclerAdapter.java
+++ app/src/main/java/edu/neu/madcourse/topdog/RecyclerAdapter.java
@@ -29,8 +29,8 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerHolder> {
     @Override
     public void onBindViewHolder(RecyclerHolder holder, int position) {
         Walk currentItem = walkHistoryList.get(position);
-        holder.walkDuration.setText(String.valueOf(currentItem.getWalkDuration()));
-        holder.walkDistance.setText(String.valueOf(currentItem.getFinalDistance()));
+        holder.walkDuration.setText(String.valueOf(currentItem.getWalkDuration()) + " minutes");
+        holder.walkDistance.setText(String.valueOf(currentItem.getFinalDistance()) + " meters");
         holder.walkCount.setText("Walk " + (position+1));
     }
 
